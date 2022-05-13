8cc4d6b71ef86edf0c531f6a5866fc0da4ce588d
darkjb <j.blanko.b@gmail.com>
2022-05-13 20:23:07 +0200

    PEC 2 (build)

---
+++ dist/index.ccc3197f.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".ir-arriba").click((function(){$("body, html").animate({scrollTop:"0px"},300)})),$(window).scroll((function(){$(this).scrollTop()>0?$(".ir-arriba").slideDown(300):$(".ir-arriba").slideUp(300)}))}));
+//# sourceMappingURL=index.ccc3197f.js.map
