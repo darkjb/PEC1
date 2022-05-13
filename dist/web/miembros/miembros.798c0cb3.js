94c763db43aaf4f71afa7f2f543812c325b9cfa1
darkjb <j.blanko.b@gmail.com>
2022-05-13 20:45:44 +0200

    PEC 2 (arreglar problemes WCAG)

--- dist/web/miembros/miembros.798c0cb3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementsByClassName("carousel-left-arrow")[0],a=document.getElementsByClassName("carousel-right-arrow")[0];new Carousel({panels:document.getElementsByClassName("carousel-panel"),leftArrow:e,rightArrow:a,arrowDisabledClass:"arrow-disabled"}).goTo(0)}();
-//# sourceMappingURL=miembros.798c0cb3.js.map
